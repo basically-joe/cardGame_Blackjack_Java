90647a6af7a7437147405bbe81bcee5e6f5fe775
Joe McColl <joe4kool@gmail.com>
2019-02-14 13:57:35 +0000

    initial commit, card class, enums for rank and suit testing passes

---
+++ src/test/java/CardTest.java
@@ -0,0 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class CardTest {
+
+	Card card;
+
+	@Before
+	public void before(){
+		card = new Card(SuitType.HEARTS, RankType.KING);
+	}
+
+	@Test
+	public void getSuit(){
+		assertEquals(SuitType.HEARTS, card.getSuit());
+	}
+}
